75e43459c575859dcd1b8e3ec97bfecd677696a2
Matthew Smith <matthew.smith121@hotmail.ca>
2020-04-16 15:59:43 -0400

    Switched to LWRP
    Added LWRP so I could use the Shader Graph
    Added a dissolve effect to pedestal items

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,8 +4,14 @@
 public class PlayerMovement : MonoBehaviour
 {
     public float speed = 8f;
+    public float gravity = -9.81f;
 
     Rigidbody playerRigidbody;
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public float jumpSpeed = 3f;
+    public LayerMask groundMask;
+    bool isGrounded;
 
     private void Start() {
         playerRigidbody = GetComponent<Rigidbody>(); 
@@ -14,8 +20,14 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded) {
+            // TODO: add jumping
+        }
 
         Vector3 move = (transform.right * x + transform.forward * z).normalized * speed * Time.deltaTime;
 
