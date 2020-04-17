fed6717690d09c4a5afddcf33dcc4818317c1a4b
Matthew Smith <matthew.smith121@hotmail.ca>
2020-04-17 12:08:56 -0400

    Player Jumping
    Finally got player jumping
    Had to cave in and use character controller, which means I had to do a bit of jank to get the entity to still have collision since the character controller doesn't convert

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,6 +3,12 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour, IConvertGameObjectToEntity
 {
+    void Start() {
+        // Turn off box collider so it doesn't interfere with CharacterController
+        // still need it though for entity conversion
+        GetComponent<BoxCollider>().enabled = false;
+    }
+
     public void Convert(Entity entity, EntityManager dstManager, GameObjectConversionSystem conversionSystem) {
         dstManager.AddComponent(entity, typeof(PlayerTag));
     }
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,15 +6,17 @@ public class PlayerMovement : MonoBehaviour
     public float speed = 8f;
     public float gravity = -9.81f;
 
-    Rigidbody playerRigidbody;
+    CharacterController controller;
     public Transform groundCheck;
     public float groundDistance = 0.4f;
     public float jumpSpeed = 3f;
     public LayerMask groundMask;
     bool isGrounded;
 
+    Vector3 velocity;
+
     private void Start() {
-        playerRigidbody = GetComponent<Rigidbody>(); 
+        controller = GetComponent<CharacterController>(); 
     }
 
     // Update is called once per frame
@@ -22,15 +24,23 @@ public class PlayerMovement : MonoBehaviour
     {
         isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
 
+        if (isGrounded && velocity.y < 0) {
+            velocity.y = -2f;
+        }
+
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
-        if (Input.GetKeyDown(KeyCode.Space) && isGrounded) {
-            // TODO: add jumping
+        if (Input.GetButtonDown("Jump") && isGrounded) {
+            velocity.y = Mathf.Sqrt(jumpSpeed * -2f * gravity);
         }
+
+        velocity.y += gravity * Time.deltaTime;
 
         Vector3 move = (transform.right * x + transform.forward * z).normalized * speed * Time.deltaTime;
 
-        playerRigidbody.MovePosition(transform.position + move);
+        controller.Move(move);
+
+        controller.Move(velocity * Time.deltaTime);
     }
 }
