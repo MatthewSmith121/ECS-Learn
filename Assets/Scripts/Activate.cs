283cd92b67e8f4fd5e8e1442c7d4c7017a0cd280
Matthew Smith <matthew.smith121@hotmail.ca>
2020-04-17 13:55:11 -0400

    Can Activate Magic Items
    Aproaching a magic item on one of the pedestals will bring up a prompt to "Press E" and if pressed will cause the magic item to dissolve

---
+++ Assets/Scripts/Activate.cs
@@ -0,0 +1,51 @@
+﻿
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Activate : MonoBehaviour
+{
+    public Text activateText;
+    public GameObject magicItem;
+    Material dissolveMaterial;
+    bool inRange;
+    bool dissolve;
+    float dissolveAmount;
+    const float dissolveSpeed = 1f;
+
+    private void Start() {
+        dissolveMaterial = magicItem.GetComponent<Renderer>().material;
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        Debug.Log("Enter");
+        activateText.enabled = true;
+        inRange = true;
+    }
+
+    private void OnTriggerExit(Collider other) {
+        Debug.Log("Exit");
+        activateText.enabled = false;
+        inRange = false;
+    }
+
+    private void Update() {
+        if (dissolve) {
+            if (dissolveAmount >= 1f) {
+                dissolve = false;
+            }
+            else {
+                dissolveAmount += dissolveSpeed * Time.deltaTime;
+                dissolveMaterial.SetFloat("_DissolveAmount", dissolveAmount);
+            }
+        }
+
+        if (!inRange) {
+            return;
+        }   
+
+        if (Input.GetKeyDown(KeyCode.E)) {
+            //magicItem.SetActive(false);
+            dissolve = true;
+        }
+    }
+}
